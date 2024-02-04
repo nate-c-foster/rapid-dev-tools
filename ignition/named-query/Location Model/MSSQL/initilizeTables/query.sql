0a7a77a9bb4830871d4f7e541f669df34423d1ef
nate-c-foster <nate.c.foster@gmail.com>
2024-02-04 14:25:46 -0600

    Update: add defaults to tabel initilizer

--- ignition/named-query/Location Model/MSSQL/initilizeTables/query.sql
+++ ignition/named-query/Location Model/MSSQL/initilizeTables/query.sql
@@ -25,9 +25,9 @@ BEGIN
 			[LocationID] [int] IDENTITY(60,1) NOT NULL,
 			[ParentLocationID] [int] NULL,
 			[LocationTypeID] [int] NULL,
-			[Name] [nvarchar](50) NOT NULL,
+			[Name] [nvarchar](50) NOT NULL DEFAULT 'New Name',
 			[Description] [nvarchar](max) NULL,
-			[isActive] [bit] NULL,
+			[isActive] [bit] NULL DEFAULT 1,
 			[LastModifiedBy] [nvarchar](50) NULL,
 			[LastModifiedOn] [datetime] NULL,
 			[ZoomLevel] [int] NULL,
@@ -60,31 +60,29 @@ BEGIN
 
 		CREATE TABLE [core].[LocationTypeDefinition](
 			[LocationTypeDefinitionID] [int] IDENTITY(1,1) NOT NULL,
-			[Name] [nvarchar](50) NOT NULL,
+			[Name] [nvarchar](50) NOT NULL DEFAULT 'New Name',
 			[Description] [nvarchar](max) NULL,
 			[UDTPath] [nvarchar](100) NULL,
-			[isActive] [int] NULL,
+			[isActive] [int] NULL DEFAULT 1,
 			[LastModifiedBy] [nvarchar](50) NULL,
 			[LastModifiedOn] [datetime] NULL,
-			[LocationTypeID] [int] NOT NULL,
+			[LocationTypeID] [int] NOT NULL DEFAULT 1,
 			[IgnitionTemplatePath] [nvarchar](100) NULL,
 			[IgnitionPIDTemplatePath] [nvarchar](max) NULL,
 		 	CONSTRAINT [PK_LocationTypeDefinitionID] PRIMARY KEY (LocationTypeDefinitionID)
 		 	);
 
 		SET IDENTITY_INSERT [core].[LocationTypeDefinition] ON 
-		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (1, N'Enterprise', N'Root level of hierarchy', N'[SCADA]_types_/Location Types/Enterprise', 1, N'Project Initializer', getDate(), 1, N'SCADA/Templates/View/Process Stage', N'')
-		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (2, N'State', N'State entity of model', N'[SCADA]_types_/Location Types/State', 1, N'Project Initializer', getDate(), 2, N'SCADA/Templates/View/Process Stage', N'')
-		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (3, N'Water System', N'Water entity of system', N'[SCADA]_types_/Location Types/Water System', 1, N'Project Initializer', getDate(), 3, N'SCADA/Templates/View/Process Stage', N'')
-		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (4, N'Waste Water System', N'Waste Water entity of system', N'[SCADA]_types_/Location Types/Wastewater System', 1, N'Project Initializer', getDate(), 4, N'SCADA/Templates/View/Process Stage', N'')
-		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (5, N'Ground Water Plant', N'Ground Water entity ', N'[SCADA]_types_/Site/Ground Water Plant', 1, N'Project Initializer', getDate(), 5, N'SCADA/Templates/View/Process Stage', N'')
-		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (6, N'Surface Water Plant', N'Surface Water entity', N'[SCADA]_types_/Site/Surface Water Plant', 1, N'Project Initializer', getDate(), 5, N'SCADA/Templates/View/Process Stage', N'')
-		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (7, N'General Process', N'Process entity', N'[SCADA]_types_/Processes/General Process', 1, N'Project Initializer', getDate(), 6, N'SCADA/Templates/View/Process', N'')
-		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (8, N'General Process Stage', N'Process Stage entity', N'[SCADA]_types_/Process Stage/General Process Stage', 1, N'Project Initializer', getDate(), 8, N'SCADA/Templates/View/Process Stage', N'')
-		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (17, N'Military Base', N'Military Base', N'[SCADA]_types_/Location Types/Military Base', 1, N'Project Initializer', getDate(), 9, N'SCADA/Templates/View/Process Stage', N'')
-		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (19, N'Wastewater Remote Sites', N'Lift Stations ...', N'[SCADA]_types_/Site/Wastewater Remote Sites', 1, N'Project Initializer', getDate(), 5, N'SCADA/Templates/View/Process Stage', N'')
-		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (2025, N'General Water System', N'Container of Water Systems', N'[SCADA]_types_/Location Types/General Water System', 1, N'Project Initializer', getDate(), 10, N'SCADA/Templates/View/Process Stage', N'')
-		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (2026, N'Wastewater Plant', N'Wastewater Treatment Plant', N'[SCADA]_types_/Site/Wastewater Plant', 1, N'Project Initializer', getDate(), 5, N'SCADA/Templates/View/Process Stage', N'')
+		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (1, N'Enterprise', N'Root level of hierarchy', N'[SCADA]_types_/Location Types/Enterprise', 1, N'Project Initializer', getDate(), 1, N'User Defined/Views/General Location', N'')
+		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (2, N'State', N'State entity of model', N'[SCADA]_types_/Location Types/State', 1, N'Project Initializer', getDate(), 2, N'User Defined/Views/General Location', N'')
+		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (3, N'Water System', N'Water entity of system', N'[SCADA]_types_/Location Types/Water System', 1, N'Project Initializer', getDate(), 3, N'User Defined/Views/General Location', N'')
+		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (4, N'Waste Water System', N'Waste Water entity of system', N'[SCADA]_types_/Location Types/Wastewater System', 1, N'Project Initializer', getDate(), 4, N'User Defined/Views/General Location', N'')
+		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (5, N'Ground Water Plant', N'Ground Water entity ', N'[SCADA]_types_/Site/Ground Water Plant', 1, N'Project Initializer', getDate(), 5, N'User Defined/Views/General Location', N'')
+		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (6, N'Surface Water Plant', N'Surface Water entity', N'[SCADA]_types_/Site/Surface Water Plant', 1, N'Project Initializer', getDate(), 5, N'User Defined/Views/General Locatione', N'')
+		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (7, N'General Process', N'Process entity', N'[SCADA]_types_/Processes/General Process', 1, N'Project Initializer', getDate(), 6, N'User Defined/Views/General Process', N'')
+		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (8, N'General Process Stage', N'Process Stage entity', N'[SCADA]_types_/Process Stage/General Process Stage', 1, N'Project Initializer', getDate(), 8, N'User Defined/Views/General Location', N'')
+		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (9, N'Military Base', N'Military Base', N'[SCADA]_types_/Location Types/Military Base', 1, N'Project Initializer', getDate(), 9, N'User Defined/Views/General Location', N'')
+		INSERT [core].[LocationTypeDefinition] ([LocationTypeDefinitionID], [Name], [Description], [UDTPath], [isActive], [LastModifiedBy], [LastModifiedOn], [LocationTypeID], [IgnitionTemplatePath], [IgnitionPIDTemplatePath]) VALUES (10, N'Wastewater Plant', N'Wastewater Treatment Plant', N'[SCADA]_types_/Site/Wastewater Plant', 1, N'Project Initializer', getDate(), 5, N'User Defined/Views/General Location', N'')
 		SET IDENTITY_INSERT [core].[LocationTypeDefinition] OFF
 
 	END
@@ -99,10 +97,10 @@ BEGIN
 
 		CREATE TABLE [core].[LocationType](
 			[LocationTypeID] [int] IDENTITY(1,1) NOT NULL,
-			[Name] [nvarchar](50) NOT NULL,
+			[Name] [nvarchar](50) NOT NULL DEFAULT 'New Name',
 			[Description] [nvarchar](max) NULL,
 			[Icon] [nvarchar](50) NULL,
-			[isActive] [bit] NULL,
+			[isActive] [bit] NULL DEFAULT 1,
 			[LastModifiedBy] [nvarchar](50) NULL,
 			[LastModifiedOn] [datetime] NULL,
 		 CONSTRAINT [PK_LocationTypeID] PRIMARY KEY (LocationTypeID)
@@ -118,7 +116,6 @@ BEGIN
 		INSERT [core].[LocationType] ([LocationTypeID], [Name], [Description], [Icon], [isActive], [LastModifiedBy], [LastModifiedOn]) VALUES (7, N'Component', N'Component within a process', N'material/settings', 1, N'Project Initializer', getDate())
 		INSERT [core].[LocationType] ([LocationTypeID], [Name], [Description], [Icon], [isActive], [LastModifiedBy], [LastModifiedOn]) VALUES (8, N'Process Stage', N'Process Stage', N'flexware/process_group', 1, N'Project Initializer', getDate())
 		INSERT [core].[LocationType] ([LocationTypeID], [Name], [Description], [Icon], [isActive], [LastModifiedBy], [LastModifiedOn]) VALUES (9, N'Military Base', N'Military Service Site', N'', 1, N'Project Initializer', getDate())
-		INSERT [core].[LocationType] ([LocationTypeID], [Name], [Description], [Icon], [isActive], [LastModifiedBy], [LastModifiedOn]) VALUES (10, N'General Water System', N'Container of Water Systems', N'', 1, N'Project Initializer', getDate())
 		SET IDENTITY_INSERT [core].[LocationType] OFF
 		
 	END
